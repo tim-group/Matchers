72b7e04d83ef047a5604f925fc26b5b8dc8b99fc
grahamcarlyle <graham@grahamcarlyle.com>
2011-09-30 15:24:07 +0000

    GrahamC: use MatcherMatcher in tests for StringContainsSequence matcher
    git-svn-id: http://srcctrl.youdevise.com/opt/repo/projects/Matchers/trunk@81912 7edc9554-746b-495c-92d9-cd406c1090b3

--- src/test/java/com/youdevise/testutils/matchers/StringContainsSequenceTest.java
+++ src/test/java/com/youdevise/testutils/matchers/StringContainsSequenceTest.java
@@ -1,9 +1,12 @@
 package com.youdevise.testutils.matchers;
 
+import static com.youdevise.testutils.matchers.MatcherMatcher.a_matcher_giving_a_mismatch_description_of;
+import static com.youdevise.testutils.matchers.MatcherMatcher.a_matcher_that_matches;
+import static com.youdevise.testutils.matchers.MatcherMatcher.a_matcher_with_description;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
-import org.hamcrest.StringDescription;
 import org.junit.Test;
 
 
@@ -12,27 +15,24 @@ public class StringContainsSequenceTest {
     @Test public void
     matches_strings_containing_the_sequence_of_strings() {
         StringContainsSequence sequenceMatcher = new StringContainsSequence("a", "b", "c");
-        assertThat(sequenceMatcher.matches("abc"), is(true));
-        assertThat(sequenceMatcher.matches("- a b c -"), is(true));
-        assertThat(sequenceMatcher.matches("bca"), is(false));
-        assertThat(sequenceMatcher.matches("ab"), is(false));
-    }
-
-    @Test public void
-    describes_itself_properly() {
-        StringContainsSequence sequenceMatcher = new StringContainsSequence("a", "b", "c");
-        StringDescription description = new StringDescription();
-        sequenceMatcher.describeTo(description);
-        assertThat(description.toString(), is("a string contains the sequence of substrings [\"a\", \"b\", \"c\"]"));
+        assertThat(sequenceMatcher, is(a_matcher_that_matches("abc")));
+        assertThat(sequenceMatcher, is(a_matcher_that_matches("- a b c -")));
     }
     
     @Test public void
-    describes_a_mismatch() {
-        StringContainsSequence sequenceMatcher = new StringContainsSequence("a", "z");
-        StringDescription description = new StringDescription();
-        sequenceMatcher.describeMismatch("foo", description); 
-        assertThat(description.toString(), is("was \"foo\""));
+    describes_mismatches() {
+        StringContainsSequence sequenceMatcher = new StringContainsSequence("a", "b", "c");
+        assertThat(sequenceMatcher, is(a_matcher_giving_a_mismatch_description_of("", equalTo("was \"\""))));
+        assertThat(sequenceMatcher, is(a_matcher_giving_a_mismatch_description_of("bca", equalTo("was \"bca\""))));
+        assertThat(sequenceMatcher, is(a_matcher_giving_a_mismatch_description_of("ab", equalTo("was \"ab\""))));
     }
 
+    @Test public void
+    describes_itself() {
+        StringContainsSequence sequenceMatcher = new StringContainsSequence("a", "b", "c");
+        assertThat(sequenceMatcher, 
+                   is(a_matcher_with_description(equalTo("a string contains the sequence of substrings [\"a\", \"b\", \"c\"]"))));
+    }
+    
 }
 
