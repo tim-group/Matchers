e3b0b51915f4779f64fc3192d28ee6d446acfa91
Graham Allan <Graham.Allan@youdevise.com>
2012-04-12 10:36:53 +0100

    GrahamA: add a SimilarPropertyValuesMatcher, which allows ignoring properties.

---
+++ src/main/java/com/youdevise/testutils/matchers/beans/SimilarPropertyValuesMatcher.java
@@ -0,0 +1,112 @@
+package com.youdevise.testutils.matchers.beans;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Sets;
+import com.youdevise.testutils.matchers.beans.PropertyMatcher;
+
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+import org.hamcrest.Matcher;
+import org.hamcrest.StringDescription;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
+
+import static com.google.common.collect.Iterables.filter;
+import static com.youdevise.testutils.matchers.beans.PropertyMatcher.an_object_whose;
+import static java.util.Arrays.asList;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.beans.PropertyUtil.NO_ARGUMENTS;
+import static org.hamcrest.beans.PropertyUtil.propertyDescriptorsFor;
+
+public class SimilarPropertyValuesMatcher<T> extends TypeSafeDiagnosingMatcher<T> {
+
+    private final Set<String> ignoredProperties = Sets.newHashSet();
+    private final T expected;
+    
+    private SimilarPropertyValuesMatcher(T expected) {
+        this.expected = expected;
+    }
+
+    @Factory
+    public static <T> SimilarPropertyValuesMatcher<T> similar_properties_as(T expected) {
+        return new SimilarPropertyValuesMatcher<T>(expected);
+    }
+    
+    public SimilarPropertyValuesMatcher<T> ignoring(String... propertyNames) {
+        this.ignoredProperties.addAll(Arrays.asList(propertyNames));
+        return this;
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText("an object ");
+        
+        for (PropertyMatcher matcher : propertyMatchersFor(expected)) {
+            description.appendDescriptionOf(matcher);
+        }
+        
+        description.appendText("ignoring properties ").appendValueList("[", ", ", "]", ignoredProperties);
+    }
+
+    @Override
+    protected boolean matchesSafely(T actual, Description mismatchDescription) {
+        final List<Description> mismatchDescriptions = new ArrayList<Description>();
+        
+        boolean matched = true;
+        for (PropertyMatcher matcher : propertyMatchersFor(actual)) {
+            if (matcher.matches(actual)) {
+                continue;
+            }
+            
+            matched = false;
+            Description thisMismatch = new StringDescription();
+            mismatchDescriptions.add(thisMismatch);
+            matcher.describeMismatch(actual, thisMismatch);
+        }
+        
+        mismatchDescription.appendValueList("", ", ", "", mismatchDescriptions);
+        
+        return matched;
+    }
+
+    private List<PropertyMatcher> propertyMatchersFor(T actual) {
+        List<PropertyMatcher> matchers = new ArrayList<PropertyMatcher>();
+        
+        for (PropertyDescriptor property : readablePropertyDescriptorsFor(actual)) {
+            if (!ignoredProperties.contains(property.getDisplayName())) {
+                Matcher<?> matcher = matcherFor(property);
+                        
+                matchers.add(an_object_whose(property.getDisplayName(), matcher));
+            }
+        }
+
+        return matchers;
+    }
+
+    private Matcher<?> matcherFor(PropertyDescriptor property) {
+        return equalTo(readProperty(property.getReadMethod(), expected));
+    }
+
+    private Iterable<PropertyDescriptor> readablePropertyDescriptorsFor(T actual) {
+        return filter(asList(propertyDescriptorsFor(actual, null)), HAS_GETTER);
+    }
+
+    private static final Predicate<PropertyDescriptor> HAS_GETTER = new Predicate<PropertyDescriptor>() {
+        @Override public boolean apply(PropertyDescriptor input) { return input.getReadMethod() != null; }
+    };
+
+    private static Object readProperty(Method method, Object target) {
+        try {
+          return method.invoke(target, NO_ARGUMENTS);
+        } catch (Exception e) {
+          throw new IllegalArgumentException("Could not invoke " + method + " on " + target, e);
+        }
+      }
+
+}
---
+++ src/test/java/com/youdevise/testutils/matchers/beans/SimilarPropertyValuesMatcherTest.java
@@ -0,0 +1,116 @@
+package com.youdevise.testutils.matchers.beans;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import static com.youdevise.testutils.matchers.beans.SimilarPropertyValuesMatcher.similar_properties_as;
+import static com.youdevise.testutils.matchers.MatcherMatcher.a_matcher_giving_a_mismatch_description_of;
+import static com.youdevise.testutils.matchers.MatcherMatcher.a_matcher_that_matches;
+import static com.youdevise.testutils.matchers.MatcherMatcher.a_matcher_with_description;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+
+public class SimilarPropertyValuesMatcherTest {
+    
+    @Test public void
+    matches_when_all_properties_are_the_same_with_no_exclusions() {
+         MyBean first = new MyBean("a", 1, Arrays.asList(2, 3, 4));
+         MyBean second = new MyBean("a", 1, Arrays.asList(2, 3, 4));
+         
+         assertThat(similar_properties_as(first), is(a_matcher_that_matches(second)));
+    }
+    
+    @Test public void
+    allows_properties_to_be_the_same_if_they_are_excluded() {
+        MyBean first = new MyBean("a", 1, Arrays.asList(2, 3, 4));
+        MyBean second = new MyBean("a", 1, Arrays.asList(2, 3, 4));
+        
+        assertThat(similar_properties_as(first).ignoring("myString"), is(a_matcher_that_matches(second)));
+    }
+
+    @Test public void
+    allows_properties_to_be_different_if_they_are_excluded() {
+        MyBean first = new MyBean("a", 1, Arrays.asList(2, 3, 4));
+        MyBean second = new MyBean("b", 1, Arrays.asList(2, 3, 4));
+        
+        assertThat(similar_properties_as(first).ignoring("myString"), is(a_matcher_that_matches(second)));
+    }
+
+    @Test public void
+    describes_mismatch_where_single_property_is_different() {
+        MyBean first = new MyBean("a", 1, Arrays.asList(2, 3, 4));
+        MyBean second = new MyBean("b", 1, Arrays.asList(2, 3, 4));
+        
+        assertThat(similar_properties_as(first), is(a_matcher_giving_a_mismatch_description_of(second, is("<whose myString was \"b\">"))));
+    }
+
+    @Test public void
+    describes_mismatch_where_multiple_properties_are_different() {
+        MyBean first = new MyBean("a", 1, Arrays.asList(2, 3, 4));
+        MyBean second = new MyBean("b", 2, Arrays.asList(2, 3, 4));
+        
+        assertThat(similar_properties_as(first), is(a_matcher_giving_a_mismatch_description_of(second, is("<whose myInt was <2>>, <whose myString was \"b\">"))));
+    }
+
+    @Test public void
+    describes_expected() {
+        MyBean first = new MyBean("a", 1, Arrays.asList(2, 3, 4));
+        
+        assertThat(similar_properties_as(first), is(a_matcher_with_description(allOf(containsString("an object "),
+                                                                                  containsString("whose myInt <1>"),
+                                                                                  containsString("whose myList <[2, 3, 4]>"),
+                                                                                  containsString("whose myString \"a\"")))));
+    }
+    
+    @Test public void
+    does_not_try_to_read_the_write_only_properties() {
+         final WriteOnlyProperties first = new WriteOnlyProperties();
+         final WriteOnlyProperties second = new WriteOnlyProperties();
+         
+         assertThat(similar_properties_as(first), is(a_matcher_that_matches(second)));
+    }
+
+    @Test public void
+    describes_expected_including_the_list_of_ignored_properties() {
+        MyBean first = new MyBean("a", 1, Arrays.asList(2, 3, 4));
+        
+        assertThat(similar_properties_as(first).ignoring("myInt", "myString"), is(a_matcher_with_description(allOf(containsString("an object "),
+                                                                                  containsString("whose myList <[2, 3, 4]>"),
+                                                                                  containsString("ignoring properties [\"myInt\", \"myString\"]")))));
+    }
+    
+    public static class MyBean {
+        private final String myString;
+        private final int myInt;
+        private final List<? extends Number> myList;
+
+        public MyBean(String myString, int myInt, List<? extends Number> myList) {
+            this.myString = myString;
+            this.myInt = myInt;
+            this.myList = myList;
+        }
+
+        public String getMyString() {
+            return myString;
+        }
+
+        public int getMyInt() {
+            return myInt;
+        }
+
+        public List<? extends Number> getMyList() {
+            return myList;
+        }
+        
+    }
+    
+    public static class WriteOnlyProperties {
+        public void setFoo(@SuppressWarnings("unused") String foo) {  }
+        public String getMyString() { return "MyString"; }
+    }
+    
+}
