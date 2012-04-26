cc5fbd81b41eff84cdb2f0e2ab8d4ec5dc236df5
Samir Talwar <samir.talwar@timgroup.com>
2012-04-26 11:26:13 +0100

    Shaf / Samir: Moved StringRegExMatcher into Matchers.

---
+++ src/main/java/com/youdevise/testutils/matchers/StringRegexMatcher.java
@@ -0,0 +1,31 @@
+package com.youdevise.testutils.matchers;
+
+import java.util.regex.Pattern;
+
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
+
+public final class StringRegexMatcher extends TypeSafeDiagnosingMatcher<String> {
+    private final String pattern;
+
+    private StringRegexMatcher(String pattern) {
+        this.pattern = pattern;
+    }
+
+    @Factory
+    public static StringRegexMatcher matchesRegex(final String pattern) {
+        return new StringRegexMatcher(pattern);
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText("a string matching " + pattern);
+    }
+
+    @Override
+    protected boolean matchesSafely(String item, Description mismatchDescription) {
+        mismatchDescription.appendText("was " + item);
+        return Pattern.compile(".*" + pattern + ".*", Pattern.DOTALL).matcher(item).matches();
+    }
+}
