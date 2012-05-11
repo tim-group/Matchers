63b3c9102b19278542aa578a3941a608fd4ff342
Dominic Fox <dominic.fox@gmail.com>
2012-05-11 11:23:40 +0100

    Remove MagicJavaBeanMatchers (now in project stanislavski)

--- src/main/java/com/youdevise/testutils/matchers/beans/JavaBeanMatcher.java
+++
@@ -1,7 +0,0 @@
-package com.youdevise.testutils.matchers.beans;
-
-import org.hamcrest.Matcher;
-
-public interface JavaBeanMatcher<T> extends Matcher<T> {
-    void withProperty(String name, Matcher<?> matcher);
-}
