de095b07e2986384ac33f0b921998a8ccfa29f7f
Steve Haslam <steve.haslam@timgroup.com>
2019-09-05 16:04:06 +0100

    SteveH: delay task creation

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,11 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        val repoUrl: String by settings
+        maven(url = "$repoUrl/groups/public")
+    }
+}
+
 rootProject.name = "Matchers"
 
 include("http-client")
